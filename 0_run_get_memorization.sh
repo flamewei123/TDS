c973ff5342cb1d32fb2dac69e5ee857b93afcc55
Wuxw <43459224+flamewei123@users.noreply.github.com>
2025-05-14 18:36:45 +0800

    Create 0_run_get_memorization.sh

---
+++ 0_run_get_memorization.sh
@@ -0,0 +1,8 @@
+python 0_get_memorization.py \
+    --dataset_name wikitext-103 \
+    --model_path /data1/model/llama-3.2-1b \
+    --output_idx 0 \
+    --prefix_length 100 \
+    --tgt_len 10 \
+    --batch_size 64 \
+    --gpu_id 1 \
